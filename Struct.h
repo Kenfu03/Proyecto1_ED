cea22b39311e018ddd2b0a651324b258a184989d
NikoTico <52984591+nikotico@users.noreply.github.com>
2020-12-01 23:47:56 -0600

    Actualizacion

--- Struct.h
+++ Struct.h
@@ -3,16 +3,13 @@
 
 using namespace std;
 
+struct Paquetes;
 struct NodoC;
 struct NodoLista;
 struct ListaCircular;
 struct Cola;
 struct ColaPrioridad;
 struct Galleta;
-struct Paquetico;
-struct Tubo;
-struct Paquete;
-
 struct NodoC{
 	int dato; 
     NodoC* next;
@@ -43,26 +40,44 @@ struct Cola{
     void print(void);
     
 };
+
+struct Paquetes{
+	string nombre;
+	int cantGalle;
+	
+	Paquetes(string nombre, int cantGalle){
+		this->nombre = nombre;
+		this->cantGalle = cantGalle;
+	}
+	
+	void impPaquete(){
+		cout << "Tipo: " << nombre << " cantidad ->" << cantGalle;
+	}
+};
+
 struct NodoLista{
-	int dato;
+	Paquetes * paquete;
 	NodoLista * siguiente;
 	NodoLista * anterior;
 	
-	NodoLista(int d)
+	NodoLista(Paquetes * tp)
 	{
-		dato = d;
+		paquete = tp;
 		siguiente = anterior = NULL;
 	}
+	
 	NodoLista()
 	{
-		dato = 0;
+		paquete = NULL;
 		siguiente = anterior = NULL;
 	}
 	
 	void imprimir ()
 	{
-		cout << "<-|" << dato << "|-> ";
+		paquete->impPaquete();
+		cout << "\n";
 	}
+	
 };
 struct ListaCircular{
 	NodoLista * primerNodo;
@@ -70,15 +85,15 @@ struct ListaCircular{
 		primerNodo = NULL;
 	}	
 	// INSERTAR AL INICIO
-	void insertar(int d)
+	void insertarAUX(Paquetes * tipo)
 	{
 		if (primerNodo == NULL){
-			primerNodo = new NodoLista(d);
+			primerNodo = new NodoLista(tipo);
 			primerNodo->siguiente= primerNodo;
 			primerNodo->anterior = primerNodo;
 		}
 		else{
-			NodoLista * nuevo = new NodoLista (d);
+			NodoLista * nuevo = new NodoLista (tipo);
 			nuevo->siguiente = primerNodo;
 			nuevo->anterior = primerNodo->anterior;
 			primerNodo->anterior->siguiente = nuevo;
@@ -86,11 +101,40 @@ struct ListaCircular{
 		}
 	}
 	
+	void insertar(string nombre, int cantidad){
+		if(nombre == "Paquetico"){
+			for(int i = 0;i<cantidad;i++){
+				insertarAUX( new Paquetes("Paquetico",4) );
+			}
+		}
+		if(nombre == "Tubo"){
+			for(int i = 0;i<cantidad;i++){
+				insertarAUX(new Paquetes(nombre,16));
+			}
+		}
+		if(nombre == "Paquete"){
+			for(int i = 0;i<cantidad;i++){
+				insertarAUX(new Paquetes(nombre,40));
+			}
+		}		
+	}
+	
+	int totalGalletas(){
+		int total = 0 ;
+		if (primerNodo != NULL){
+			NodoLista * tmp = primerNodo;
+			do{
+				total += tmp->paquete->cantGalle;
+				tmp = tmp->siguiente;
+			}while(tmp!=primerNodo);
+			cout << endl;
+			}
+		return total;
+	}
 	// IMPRIMIR LISTA
 	void imprimir()
 	{
 		if (primerNodo != NULL){
-			
 			NodoLista * tmp = primerNodo;
 			do{
 				tmp->imprimir();
@@ -100,7 +144,8 @@ struct ListaCircular{
 		}
 	}
 	
-		// IMPRIMIR LISTA
+	// IMPRIMIR LISTA
+	/*
 	NodoLista * buscar(int _dato)
 	{
 		if (primerNodo != NULL){
@@ -134,74 +179,49 @@ struct ListaCircular{
 		}
 		return eliminado;
 	}
+	*/
 };
-struct Galleta{
+struct Receta{
 	int masa;
 	int choco;
-	Galleta(int masa, int choco){
+	Receta(int masa, int choco){
 		this->masa = masa;
 		this->choco = choco;
 	}
 };
-struct Paquetico{
-	int cantidad;
-	Paquetico(){
-	}
-	void add(){
-		cantidad++;
-	}
-	bool listo(){
-		if(cantidad == 4){
-			return true;
-		}
-		return false;
-	}
-};
-struct Tubo{
-	int cantidad;
-	Tubo(){
-	}
-	void add(){
-		cantidad++;
-	}
-	bool listo(){
-		if(cantidad == 16){
-			return true;
-		}
-		return false;
-	}
-};
-struct Paquete{
-	int cantiPaqueticos;
-	Paquete(){
-	}
-	void add(){
-		cantiPaqueticos++;
-	}
-	bool listo(){
-		if(cantiPaqueticos == 10){
-			return true;
-		}
-		return false;
-	}
-};
+
 struct planificador{
-	//Define la cantidad de paquetes que se van a crear
-	int pk;
-	int litpk;
-	int tb;
-	planificador(int pk,int litpk,int tb){
-		this->pk = pk;
-		this->litpk = litpk;
-		this->tb = tb;
+	ListaCircular * lc;
+	planificador(){
+		lc = new ListaCircular();
+		lc->insertar("Paquetico",100);
+		lc->insertar("Tubo",250);
 	}
-	int TotalGalletas(){
-		return (4 * pk)+(4 * litpk)+(16 * tb);
+	
+	void insertarMas(){
+		//Aqui debe de tener algo en pantalla que me pida el tipo y la canitidad a sumar;
+		lc->insertar("Paquete",4);
 	}
 };
-struct Mezcladora{
+//El carrito debe de ser configurable en sus cantidades y este debe de ser un hilo
+struct Mez{
 	int min;
 	int max;
-	
+	Mez(int min, int max){
+		this->min = min;
+		this->max = max;
+	}
+};
+struct MezChocolate{
+	int min;
+	int max;
+	MezChocolate(int min, int max){
+		this->min = min;
+		this->max = max;
+	}
 };
 
+
+struct Ensambladora{
+	
+};
--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	Cola * col = new Cola();
+	/*Cola * col = new Cola();
 	col->push(1);
 	col->push(2);
 	col->push(3);
@@ -11,5 +11,11 @@ int main(int argc, char** argv) {
 	col->push(5);
 	col->print();
 	
+	ListaCircular * lc = new ListaCircular();
+	lc->insertar("Paquetico",100);
+	lc->insertar("Tubo",250);
+	lc->insertar("Paquete",4);
+	int galletas = lc->totalGalletas();
+	cout << "Total de galletas: " << galletas;*/
 	return 0;
 }
